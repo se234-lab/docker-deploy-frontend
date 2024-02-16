46fd1d00466cf61590d85fbc6a013300ec539ccd
BLUECODE <baek@Macintosh.local>
2024-02-16 08:56:51 +0700

    TP7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // graphqlUri: 'http://52.74.162.217:8085/graphql'
-  graphqlUri: 'http://3.84.46.137:8085/graphql'
+  graphqlUri: 'http://3.81.69.66:8085/graphql'
 
 };
