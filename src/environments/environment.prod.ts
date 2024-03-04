e765b7cf00aafd07d82081c355aa631fdd952cb1
Pipe-lines <pipe-lines>
2024-03-05 01:33:33 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // graphqlUri: 'http://52.74.162.217:8085/graphql' / ->enter your public ip
-  graphqlUri: 'http://54.234.133.207:8085/graphql'
+  graphqlUri: 'http://54.159.31.114:8085/graphql'
 
-  
+
 };
