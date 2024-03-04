091975fe90b1e02fde00f2bc743a758fc2c3f421
Pipe-lines <pipe-lines>
2024-03-05 00:16:07 +0700

    graphl fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // graphqlUri: 'http://52.74.162.217:8085/graphql'
-  graphqlUri: 'http://3.81.69.66:8085/graphql'
+  // graphqlUri: 'http://52.74.162.217:8085/graphql' / ->enter your public ip
+  graphqlUri: 'http://54.234.133.207:8085/graphql'
 
 };
