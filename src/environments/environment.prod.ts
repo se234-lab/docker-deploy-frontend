3a5366ae614b6ddd7b731cb3e9095f427e84a07e
Pipe-lines <pipe-lines>
2024-03-05 01:21:06 +0700

    TP15

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // graphqlUri: 'http://52.74.162.217:8085/graphql'
-  graphqlUri: 'http://54.234.133.207/graphql'
+  graphqlUri: 'http://54.159.31.114/graphql'
 
 };
